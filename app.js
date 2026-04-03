769adfe364c94f2ab2c9ebc21b1f2b0f20ae71ed
Deirdre Cavins <dcavins53@gmail.com>
2026-04-03 11:38:44 -0500

    test

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+//var
+
+const user1Name = "Ada";
+let user1Score = 10;
+const user2Name = "Grace";
+let user2Score = 20;
+console.log(user1.userName + " " + user1.Score)
+console.log(user1.Name, user1.userScore)
+const user1 = {  //object literal
+userName: "Ada", 
+user1Score: 10 
+
+}
+ const user2 = {
+    userName: "Grace",
+    userScore: 20,
+ }
+user1Score =user1Score + 5;
+user1Score =user1Score + 5;
+user1Score =user1Score + 5;
+console.log(user1Name, user1Score)
+
